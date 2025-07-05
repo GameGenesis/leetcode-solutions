f68ae526b618d58f45cfbcb075e57f88ac62bbc2
Rayan Kaissi <52415147+GameGenesis@users.noreply.github.com>
2025-07-05 19:03:11 -0400

    [594] [Time Beats: 88.85%] [Memory Beats: 52.77%] - LeetPush

--- solutions/Longest-Harmonious-Subsequence.py
+++ solutions/Longest-Harmonious-Subsequence.py
@@ -1,16 +1,16 @@
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        nums.sort()
-        print(nums)
-        l, r = 0, 1
+        if len(nums) == 1:
+            return 0
+        
+        freqMap = Counter(nums)
         maxLen = 0
 
-        while r < len(nums):
-            while l < r and nums[r] - nums[l] > 1:
-                l += 1
+        if len(freqMap) == 1:
+            return 0
 
-            if nums[r] - nums[l] == 1:
-                maxLen = max(maxLen, r - l + 1)
-            r += 1
+        for num, freq in freqMap.items():
+            if (num - 1) in freqMap:
+                maxLen = max(maxLen, freq + freqMap[num-1])
         
         return maxLen
